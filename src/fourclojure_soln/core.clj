759bed68525282b32ab7694f355393ae0e8a9edb
Danielwang405 <danielwang405@gmail.com>
2020-02-17 23:54:55 -0500

    signs of life! prob 44

--- src/fourclojure_soln/core.clj
+++ src/fourclojure_soln/core.clj
@@ -198,4 +198,29 @@
   (if (zero? n) 1 (* n (fact (dec n))))
 )
 
+;; 44 Rotate Sequence
 
+;; Helpers
+(defn rotate-left
+  "rotates a sequence left once"
+  [seq]
+  (concat (rest seq) (list (first seq)))  
+)
+
+(defn rotate-right
+  "rotates a sequence right once"
+  [seq]
+  (concat (list (last seq)) (butlast seq) )
+)
+
+
+;; Rotate Sequence
+(defn rotate
+  "rotates a sequence in either direction"
+  [dir seq]
+  (cond
+    (zero? dir) seq
+    (neg? dir) (rotate (inc dir) (rotate-right seq))
+    :else (rotate (dec dir) (rotate-left seq))
+  )
+)
--- test/fourclojure_soln/core_test.clj
+++ test/fourclojure_soln/core_test.clj
@@ -191,3 +191,14 @@
     (is (= (fact 8) 40320))
   )
 )
+
+;; #44
+(deftest rotate-test
+  (testing "Rotate sequence"
+    (is (= (rotate 2 [1 2 3 4 5]) '(3 4 5 1 2)))
+    (is (= (rotate -2 [1 2 3 4 5]) '(4 5 1 2 3)))  
+    (is (= (rotate 6 [1 2 3 4 5]) '(2 3 4 5 1)))
+    (is (= (rotate 1 '(:a :b :c)) '(:b :c :a)))
+    (is (= (rotate -4 '(:a :b :c)) '(:c :a :b)))
+  )  
+)
